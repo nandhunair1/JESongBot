e8f37d56fd4106f3c7a619035d89bcf3514aacd5
nandhunair1 <77907590+nandhunair1@users.noreply.github.com>
2021-04-04 23:01:16 +0530

    Update __init__.py

--- JESongBot/__init__.py
+++ JESongBot/__init__.py
@@ -10,4 +10,4 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-Jebot = Client("💓മേരി ചേച്ചി💓Bot", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
+Jebot = Client("marymoody", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
