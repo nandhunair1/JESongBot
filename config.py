8ad2a1cff20094eee22a6ff1dfd9b35d929e8dea
nandhunair1 <77907590+nandhunair1@users.noreply.github.com>
2021-07-24 13:17:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # MrC《》VENOM <https://t.me/MrC_VENOM>
 
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID","3428664"))
+API_HASH = os.getenv("API_HASH","2dad90e26708f660d81c48b1871a259e")
+BOT_TOKEN = os.getenv("BOT_TOKEN","1899594847:AAEJnJ2FfzyDCr_7rGIKVFtGdgjUqtmZUUU")
