f0059943493bb6fff82c602afa8f724b7f884da7
nandhunair1 <77907590+nandhunair1@users.noreply.github.com>
2021-07-24 13:17:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,7 @@
 
 import os
 API_ID = int(os.getenv("API_ID","3428664"))
+
 API_HASH = os.getenv("API_HASH","2dad90e26708f660d81c48b1871a259e")
+
 BOT_TOKEN = os.getenv("BOT_TOKEN","1899594847:AAEJnJ2FfzyDCr_7rGIKVFtGdgjUqtmZUUU")
